f1388fb4abe647aabd4f727822fba21e735b61d8
eduardomaximo <eduardo.santosmaximo@gmail.com>
2021-09-12 20:15:34 -0300

    expand compareProduct function on ProductsList.js

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,18 +2,39 @@ import React from "react";
 import EditProduct from "./EditProduct";
 import Product from "./Product";
 import RemoveProduct from "./RemoveProduct";
-import Sorter from "./Sorter";
+import { useSelector } from "react-redux";
 
 const ProductsList = (props) => {
+  const sortBy = useSelector((state) => state.sorter.sortBy);
+
   const compareProducts = (a, b) => {
-    if (a.name > b.name) {
-      return -1;
+    if (sortBy === "name") {
+      if (a.name > b.name) {
+        return -1;
+      }
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name === b.name) {
+        return 0;
+      }
     }
-    if (a.name < b.name) {
-      return 1;
+    if (sortBy === "price") {
+      return a.price - b.price;
     }
-    if (a.name === b.name) {
-      return 0;
+    if (sortBy === "fabrication") {
+      let x = new Date(a.fabricationDate);
+      let y = new Date(b.fabricationDate);
+
+      return x - y;
+    }
+    if (sortBy === "expiration") {
+      let x = new Date(a.goodThrough);
+      let y = new Date(b.goodThrough);
+
+      return x - y;
+    }
+    if (sortBy === "perishable") {
     }
   };
 
@@ -23,36 +44,34 @@ const ProductsList = (props) => {
 
   return (
     <ul>
-      <Sorter>
-        {sortedProducts.map((product) => {
-          return (
-            <div key={product.id}>
-              <Product
-                id={product.id}
-                name={product.name}
-                fabricationDate={product.fabricationDate}
-                goodThrough={product.goodThrough}
-                perishable={product.perishable}
-                price={product.price}
-              />
-              <EditProduct
-                id={product.id}
-                name={product.name}
-                fabricationDate={product.fabricationDate}
-                goodThrough={product.goodThrough}
-                perishable={product.perishable}
-                price={product.price}
-                setUpdateProducts={props.setUpdateProducts}
-              />
-              <RemoveProduct
-                idToRemove={product.id}
-                nameToRemove={product.name}
-                setUpdateProducts={props.setUpdateProducts}
-              />
-            </div>
-          );
-        })}
-      </Sorter>
+      {sortedProducts.map((product) => {
+        return (
+          <div key={product.id}>
+            <Product
+              id={product.id}
+              name={product.name}
+              fabricationDate={product.fabricationDate}
+              goodThrough={product.goodThrough}
+              perishable={product.perishable}
+              price={product.price}
+            />
+            <EditProduct
+              id={product.id}
+              name={product.name}
+              fabricationDate={product.fabricationDate}
+              goodThrough={product.goodThrough}
+              perishable={product.perishable}
+              price={product.price}
+              setUpdateProducts={props.setUpdateProducts}
+            />
+            <RemoveProduct
+              idToRemove={product.id}
+              nameToRemove={product.name}
+              setUpdateProducts={props.setUpdateProducts}
+            />
+          </div>
+        );
+      })}
     </ul>
   );
 };
